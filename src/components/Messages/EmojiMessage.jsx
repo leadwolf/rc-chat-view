ca459a90af97e97d162278e5ad447da7cdfe6d19
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-08-26 18:18:33 +0200

    formatted and renamed components

---
+++ src/components/Messages/EmojiMessage.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const EmojiMessage = props => {
+    return <div className="sc-message--emoji">{props.data.emoji}</div>;
+};
+
+export default EmojiMessage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './styles';
 import Launcher from './components/Launcher';
-
 
 export { Launcher };
