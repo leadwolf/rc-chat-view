836b3218d6673dd7567601f33fbdfb13a6f64c05
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-02 17:30:36 +0200

    updated messageHistory

--- demo/data/messageHistory.js
+++ demo/data/messageHistory.js
@@ -1,68 +1,112 @@
+import { MESSAGE_CONTENT_TYPE_TEXT, MESSAGE_CONTENT_TYPE_EMOJI } from '../../src/types';
+
 export default [
     {
-        type: 'text',
-        author: 'me',
-        data: { text: "Why don't they have salsa on the table?" },
+        id: 'dummy_message_01',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+        text: "Why don't they have salsa on the table?",
     },
     {
-        type: 'text',
-        author: 'them',
-        data: { text: 'What do you need salsa for?' },
+        id: 'dummy_message_02',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+        text: 'What do you need salsa for?',
     },
     {
-        type: 'text',
-        author: 'me',
-        data: { text: 'Salsa is now the number one condiment in America.' },
+        id: 'dummy_message_03',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+        text: 'Salsa is now the number one condiment in America.',
     },
     {
-        type: 'text',
-        author: 'them',
-        data: {
-            text:
-                "You know why? Because people like to say 'salsa.' 'Excuse me, do you have salsa?' 'We need more salsa.' 'Where is the salsa? No salsa?'",
-        },
+        id: 'dummy_message_04',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+
+        text:
+            "You know why? Because people like to say 'salsa.' 'Excuse me, do you have salsa?' 'We need more salsa.' 'Where is the salsa? No salsa?'",
     },
     {
-        type: 'text',
-        author: 'me',
-        data: {
-            text:
-                "You know it must be impossible for a Spanish person to order seltzer and not get salsa. 'I wanted seltzer, not salsa.'",
-        },
+        id: 'dummy_message_05',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+
+        text:
+            "You know it must be impossible for a Spanish person to order seltzer and not get salsa. 'I wanted seltzer, not salsa.'",
     },
     {
-        type: 'text',
-        author: 'them',
-        data: {
-            text:
-                "Don't you know the difference between seltzer and salsa?? You have the seltezer after the salsa!",
-        },
+        id: 'dummy_message_06',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+
+        text:
+            "Don't you know the difference between seltzer and salsa?? You have the seltezer after the salsa!",
     },
     {
-        type: 'text',
-        author: 'me',
-        data: { text: 'See, this should be a show. This is the show. ' },
-    },
-    { type: 'text', author: 'them', data: { text: 'What?' } },
-    { type: 'text', author: 'me', data: { text: 'This. Just talking.' } },
-    { type: 'text', author: 'them', data: { text: 'Yeah, right.' } },
-    {
-        type: 'text',
-        author: 'me',
-        data: { text: "I'm really serious. I think that's a good idea. " },
+        id: 'dummy_message_07',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+        text: 'See, this should be a show. This is the show. ',
     },
     {
-        type: 'text',
-        author: 'them',
-        data: { text: "Just talking? Well what's the show about?" },
+        id: 'dummy_message_08',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+        text: 'What?',
     },
-    { type: 'text', author: 'me', data: { text: "It's about nothing." } },
-    { type: 'text', author: 'them', data: { text: 'No story?' } },
-    { type: 'text', author: 'me', data: { text: 'No forget the story. ' } },
     {
-        type: 'text',
-        author: 'them',
-        data: { text: "You've got to have a story." },
+        id: 'dummy_message_09',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+        text: 'This. Just talking.',
     },
-    { type: 'emoji', author: 'me', data: { emoji: '😋' } },
+    {
+        id: 'dummy_message_10',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+        text: 'Yeah, right.',
+    },
+    {
+        id: 'dummy_message_11',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+        text: "I'm really serious. I think that's a good idea.",
+    },
+    {
+        id: 'dummy_message_12',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+        text: "Just talking? Well what's the show about?",
+    },
+    {
+        id: 'dummy_message_13',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+        text: "It's about nothing.",
+    },
+    {
+        id: 'dummy_message_14',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+        text: 'No story?',
+    },
+    {
+        id: 'dummy_message_15',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_1',
+        text: 'No forget the story. ',
+    },
+    {
+        id: 'dummy_message_16',
+        type: MESSAGE_CONTENT_TYPE_TEXT,
+        senderId: 'dummy_sender_2',
+        text: "You've got to have a story.",
+    },
+    {
+        id: 'dummy_message_17',
+        type: MESSAGE_CONTENT_TYPE_EMOJI,
+        senderId: 'dummy_sender_1',
+        emoji: '😋',
+    },
 ];
