9724d92461db7c3a5174f702163e02137aea053d
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-02 17:30:25 +0200

    changed messageType

--- src/types/index.js
+++ src/types/index.js
@@ -5,12 +5,12 @@ export const MESSAGE_CONTENT_TYPE_EMOJI = 'emoji';
 
 export const messageType = PropTypes.shape({
     id: PropTypes.string.isRequired,
-    ownerId: PropTypes.string.isRequired,
+    senderId: PropTypes.string.isRequired,
     username: PropTypes.string,
     avatarSrc: PropTypes.string,
     type: PropTypes.oneOf([MESSAGE_CONTENT_TYPE_TEXT, MESSAGE_CONTENT_TYPE_EMOJI]).isRequired,
     text: PropTypes.string,
-    emoji: PropTypes.object,
+    emoji: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 });
 
 export const messageArrayType = PropTypes.arrayOf(messageType);
