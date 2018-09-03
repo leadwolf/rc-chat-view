be1ff842a7e77c0bffb8da63327b507a0501d04d
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-03 17:26:16 +0200

    added usernames to demo messageHistory

--- demo/data/messageHistory.js
+++ demo/data/messageHistory.js
@@ -5,24 +5,28 @@ export default [
         id: 'dummy_message_01',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         text: "Why don't they have salsa on the table?",
     },
     {
         id: 'dummy_message_02',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
         text: 'What do you need salsa for?',
     },
     {
         id: 'dummy_message_03',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         text: 'Salsa is now the number one condiment in America.',
     },
     {
         id: 'dummy_message_04',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
 
         text:
             "You know why? Because people like to say 'salsa.' 'Excuse me, do you have salsa?' 'We need more salsa.' 'Where is the salsa? No salsa?'",
@@ -31,6 +35,7 @@ export default [
         id: 'dummy_message_05',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
 
         text:
             "You know it must be impossible for a Spanish person to order seltzer and not get salsa. 'I wanted seltzer, not salsa.'",
@@ -39,6 +44,7 @@ export default [
         id: 'dummy_message_06',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
 
         text:
             "Don't you know the difference between seltzer and salsa?? You have the seltezer after the salsa!",
@@ -47,66 +53,77 @@ export default [
         id: 'dummy_message_07',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         text: 'See, this should be a show. This is the show. ',
     },
     {
         id: 'dummy_message_08',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
         text: 'What?',
     },
     {
         id: 'dummy_message_09',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         text: 'This. Just talking.',
     },
     {
         id: 'dummy_message_10',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
         text: 'Yeah, right.',
     },
     {
         id: 'dummy_message_11',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         text: "I'm really serious. I think that's a good idea.",
     },
     {
         id: 'dummy_message_12',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
         text: "Just talking? Well what's the show about?",
     },
     {
         id: 'dummy_message_13',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         text: "It's about nothing.",
     },
     {
         id: 'dummy_message_14',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
         text: 'No story?',
     },
     {
         id: 'dummy_message_15',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         text: 'No forget the story. ',
     },
     {
         id: 'dummy_message_16',
         type: MESSAGE_CONTENT_TYPE_TEXT,
         senderId: 'dummy_sender_2',
+        username: 'chris',
         text: "You've got to have a story.",
     },
     {
         id: 'dummy_message_17',
         type: MESSAGE_CONTENT_TYPE_EMOJI,
         senderId: 'dummy_sender_1',
+        username: 'sam',
         emoji: ':yum:',
     },
 ];
