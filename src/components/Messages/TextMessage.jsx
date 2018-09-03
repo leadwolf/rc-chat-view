bef90836b03f8c4fb3f86ce686b39bdbdcce4bc0
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-03 16:14:21 +0200

    use markdown-it & emoji-mart to set TextMessage html

--- src/components/Messages/TextMessage.jsx
+++ src/components/Messages/TextMessage.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { Emoji as EmojiMartEmoji } from 'emoji-mart';
 
-const TextMessage = ({ text }) => <div className="sc-message--text">{text}</div>;
+const MarkdownIt = require('markdown-it');
+const emoji = require('markdown-it-emoji');
+
+const md = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+    breaks: true,
+});
+md.use(emoji);
+md.renderer.rules.emoji = (tokens, idx) =>
+    ReactDOMServer.renderToString(<EmojiMartEmoji emoji={tokens[idx].markup} size={25} />);
+
+const TextMessage = ({ text }) => {
+    const renderMarkdown = () => ({
+        __html: md.render(text),
+    });
+
+    return <div className="sc-message--text" dangerouslySetInnerHTML={renderMarkdown()} />;
+};
 
 export default TextMessage;
