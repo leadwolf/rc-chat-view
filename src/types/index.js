04f225297fd1445cecb1953af42a344e6ffd632d
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-03 18:46:08 +0200

    changed avatar prop type in messageType

--- src/types/index.js
+++ src/types/index.js
@@ -8,7 +8,22 @@ export const messageType = PropTypes.shape({
     id: PropTypes.string.isRequired,
     senderId: PropTypes.string.isRequired,
     username: PropTypes.string,
-    avatarSrc: PropTypes.string,
+    avatar: PropTypes.shape({
+        email: PropTypes.string,
+        md5Email: PropTypes.string,
+        facebookId: PropTypes.string,
+        twitterHandle: PropTypes.string,
+        instagramId: PropTypes.string,
+        googleId: PropTypes.string,
+        skypeId: PropTypes.string,
+        name: PropTypes.string,
+        maxInitials: PropTypes.string,
+        value: PropTypes.string,
+        color: PropTypes.string,
+        fgColor: PropTypes.string,
+        textSizeRatio: PropTypes.number,
+        src: PropTypes.string,
+    }),
     type: PropTypes.oneOf([MESSAGE_CONTENT_TYPE_TEXT, MESSAGE_CONTENT_TYPE_EMOJI]).isRequired,
     text: (props, propName, componentName) => {
         if (props.type === MESSAGE_CONTENT_TYPE_EMOJI) {
