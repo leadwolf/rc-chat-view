32892421527913bff834612b4cade20673d6e91c
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-03 17:43:26 +0200

    changed name in demo

--- demo/src/Header.jsx
+++ demo/src/Header.jsx
@@ -5,11 +5,9 @@ class Header extends Component {
     render() {
         return (
             <div className="demo-header">
-                <div className="demo-header--title">react-chat-window</div>
+                <div className="demo-header--title">react-chat-view</div>
                 <div className="demo-header--links">
-                    <a href="https://github.com/Christopher-Caroni/react-chat-view">
-                        Usage
-                    </a>
+                    <a href="https://github.com/Christopher-Caroni/react-chat-view">Usage</a>
                 </div>
             </div>
         );
--- demo/src/TestArea.jsx
+++ demo/src/TestArea.jsx
@@ -6,12 +6,8 @@ class TestArea extends Component {
         return (
             <div className="demo-test-area--wrapper">
                 <div className="demo-test-area--title">
-                    <div className="demo-test-area--title-main">
-                        react-chat-window demo
-                    </div>
-                    <div className="demo-test-area--title-sub">
-                        made by Christopher Caroni
-                    </div>
+                    <div className="demo-test-area--title-main">react-chat-view demo</div>
+                    <div className="demo-test-area--title-sub">made by Christopher Caroni</div>
                     <div>
                         Forked from{' '}
                         <a href="https://github.com/kingofthestack/react-chat-window">
@@ -37,23 +33,16 @@ class TestArea extends Component {
                         className="demo-test-area--text"
                         placeholder="Write a test message...."
                     />
-                    <button className="demo-test-area--button">
-                        {' '}
-                        Send Message!{' '}
-                    </button>
+                    <button className="demo-test-area--button"> Send Message! </button>
                 </form>
                 <p className="demo-test-area--info">
-                    react-chat-view is a chat window that allows you to build
-                    and add custom live chat to your sites. It includes only the
-                    react chat widget. There is no backend, and no communication
-                    system baked in.
+                    react-chat-view is a chat window that allows you to build and add custom live
+                    chat to your sites. It includes only the react chat widget. There is no backend,
+                    and no communication system baked in.
                     <br />
                     <br />
                     For instructions on how to use react-chat-window click{' '}
-                    <a href="https://github.com/Christopher-Caroni/react-chat-view">
-                        here
-                    </a>
-                    .
+                    <a href="https://github.com/Christopher-Caroni/react-chat-view">here</a>.
                 </p>
             </div>
         );
