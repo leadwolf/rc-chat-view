136761c1f19a916fda96fa59d99f599665aeb62e
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-03 15:15:46 +0200

    changed EmojiMessage to use emoji-mart emoji

--- src/components/Messages/EmojiMessage.jsx
+++ src/components/Messages/EmojiMessage.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Emoji } from 'emoji-mart';
 
-const EmojiMessage = ({ emoji }) => <div className="sc-message--emoji">{emoji}</div>;
+const EmojiMessage = ({ emoji }) => (
+    <div className="sc-message--emoji">
+        <Emoji emoji={emoji} size={32} />
+    </div>
+);
 
 export default EmojiMessage;
