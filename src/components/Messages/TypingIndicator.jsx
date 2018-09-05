16f033bad6b430c2564ff76f008de8dc713eb874
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-05 20:16:43 +0200

    created initial typingIndicator css

--- src/components/Messages/TypingIndicator.jsx
+++ src/components/Messages/TypingIndicator.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import '../../styles/messages/typingIndicator.css';
+
 /* eslint react/no-array-index-key: 0 */
 
 const TypingIndicator = ({ nDots }) => (
     <div className="typing-indicator">
         {Array.from(Array(nDots)).map((dot, index) => (
-            <span key={index} />
+            <span
+                key={index}
+                className="typing-indicator-dot"
+                style={{
+                    animation: `1s blink infinite ${(1 / nDots) * (index + 1)}s`,
+                }}
+            />
         ))}
     </div>
 );
