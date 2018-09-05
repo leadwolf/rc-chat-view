bd188cb666ab4ff53c4d0bcafcb686ed63abd404
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-05 20:42:16 +0200

    added leading dots to typing indicator

--- src/components/Messages/TypingIndicator.jsx
+++ src/components/Messages/TypingIndicator.jsx
@@ -6,16 +6,18 @@ import '../../styles/messages/typingIndicator.css';
 /* eslint react/no-array-index-key: 0 */
 
 const TypingIndicator = ({ nDots }) => (
-    <div className="typing-indicator">
-        {Array.from(Array(nDots)).map((dot, index) => (
-            <span
-                key={index}
-                className="typing-indicator-dot"
-                style={{
-                    animation: `1s blink infinite ${(1 / nDots) * (index + 1)}s`,
-                }}
-            />
-        ))}
+    <div className="typing-indicator-container">
+        <div className="typing-indicator">
+            {Array.from(Array(nDots)).map((dot, index) => (
+                <span
+                    key={index}
+                    className="typing-indicator-dot"
+                    style={{
+                        animation: `1s blink infinite ${(1 / nDots) * (index + 1)}s`,
+                    }}
+                />
+            ))}
+        </div>
     </div>
 );
 
