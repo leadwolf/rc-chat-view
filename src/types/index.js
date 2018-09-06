a6cf843184b0a831b1d89c02391db438bc1e1213
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-06 11:29:42 +0200

    commented messageType prop

--- src/types/index.js
+++ src/types/index.js
@@ -49,14 +49,14 @@ export const messageContentEmojiValidator = (props, propName, componentName) => 
 };
 
 export const messageType = PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    senderId: PropTypes.string.isRequired,
-    username: PropTypes.string,
-    avatar: avatarType,
-    type: PropTypes.oneOf([MESSAGE_CONTENT_TYPE_TEXT, MESSAGE_CONTENT_TYPE_EMOJI]).isRequired,
-    text: messageContentTextValidator,
-    emoji: messageContentEmojiValidator,
-    date: PropTypes.oneOfType([momentPropTypes.momentString, momentPropTypes.momentObj]),
+    id: PropTypes.string.isRequired, // for the react array key
+    senderId: PropTypes.string.isRequired, // used to determine whether the message was sent or received
+    username: PropTypes.string, // the username that will be displayed, if necessary
+    avatar: avatarType, // the props to pass to `react-avatar`, the avatar that will be displayed, if necessary
+    type: PropTypes.oneOf([MESSAGE_CONTENT_TYPE_TEXT, MESSAGE_CONTENT_TYPE_EMOJI]).isRequired, // the type of the message, determines which property will be used (text/emoji)
+    text: messageContentTextValidator, // the plain text to display. Markdown will be parsed automatically
+    emoji: messageContentEmojiValidator, // the emoji objet or string id, as per `emoji-mart`
+    date: PropTypes.oneOfType([momentPropTypes.momentString, momentPropTypes.momentObj]), // the date that will be displayed, if necessary
 });
 
 export const messageArrayType = PropTypes.arrayOf(messageType);
