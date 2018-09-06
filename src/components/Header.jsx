0a436507ac6ef14db2d5aaf12c707593fee3c4d6
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-06 12:09:24 +0200

    changed header css to handle when no image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,9 @@ import '../styles/header.css';
 
 const Header = ({ imageUrl, headerName, onClose }) => (
     <div className="sc-header">
-        <img className="sc-header--img" src={imageUrl} alt="" />
+        <div className="sc-header--img">
+            {imageUrl && <img className="sc-header--img" src={imageUrl} alt="" />}
+        </div>
         <div className="sc-header--team-name"> {headerName} </div>
         <div className="sc-header--close-button" onClick={onClose}>
             <img src={closeIcon} alt="" />
