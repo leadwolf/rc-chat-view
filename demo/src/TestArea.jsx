8162948655bbbaa46cc0c2c357eeb2bcf1d54167
Christopher Caroni <christopher.caroni.etu@univ-lille.fr>
2018-09-09 14:46:51 +0200

    use the new components

--- demo/src/TestArea.jsx
+++ demo/src/TestArea.jsx
@@ -3,6 +3,8 @@ import '../assets/styles/test-area.css';
 
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import MainInformation from './MainInformation';
+import SecondaryInformation from './SecondaryInformation';
 
 class TestArea extends Component {
     handleKey = event => {
@@ -26,26 +28,7 @@ class TestArea extends Component {
 
         return (
             <div className="demo-test-area--wrapper">
-                <div className="demo-test-area--title">
-                    <Typography variant="display3" color="inherit">
-                        rc-chat-ui demo
-                    </Typography>
-
-                    <Typography variant="title" color="textSecondary">
-                        made by Christopher Caroni
-                    </Typography>
-
-                    <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className="demo-test-area--info top"
-                    >
-                        Please go see the original source at{' '}
-                        <a href="https://github.com/kingofthestack/react-chat-window">
-                            kingofthestack/react-chat-window
-                        </a>
-                    </Typography>
-                </div>
+                <MainInformation />
                 <form
                     className="demo-test-area"
                     onSubmit={e => {
@@ -90,19 +73,7 @@ class TestArea extends Component {
                         Send Message!{' '}
                     </Button>
                 </form>
-                <p className="demo-test-area--info main">
-                    <Typography variant="body1" color="textPrimary" paragraph>
-                        rc-chat-view is a chat window that allows you to build and add custom live
-                        chat to your sites. <br />
-                        It includes only the react chat widget. There is no backend, and no
-                        communication system baked in.
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" paragraph>
-                        For instructions on how to use rc-chat-view click{' '}
-                        <a href="https://github.com/Christopher-Caroni/rc-chat-view#readme">here</a>
-                        .
-                    </Typography>
-                </p>
+                <SecondaryInformation />
             </div>
         );
     }
